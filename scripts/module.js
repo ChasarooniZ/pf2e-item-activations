6f53a92daa2404e2654050397175494b2462086a
cadowtin <chasdowtin@gmail.com>
2024-02-02 00:52:56 -0600

    Fix errors, comment out unneeded code

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,4 @@
-// import {generateDamageScroll, extractDamageInfoCombined, getTargetList} from './utility.js'
-// HOOKS STUFF
+
 Hooks.on("ready", () => {
     console.error("PF2e Item Activation is ready");
     if (!game.user.isGM) return;
@@ -14,38 +13,38 @@ Hooks.on("ready", () => {
     //             item
     //         }, "Item")
     //         
-    //         debugLog({ actions, isProperlyEquipped, isInvestProper }, "RemoveOrAdd");
+    //         debugLog({ actions, isProperlyEquipped, isProperlyInvested }, "RemoveOrAdd");
     //         removeOrAddActions(item.actor, actions, isProperlyEquipped && isInvestProper);
     //     }
     });
 })
 
-export function checkIfImportantUpdate(item) {
-    let actions = item.rules.filter((rule) => rule.key === "GrantItem").map(rule => rule.uuid);
-    if (actions.length === 0) return;
-    const isInvestProper = item.system.traits.value.includes("invested") === !!item.isInvested;
-    let isProperlyEquipped = false;
-    if (item.system.usage.value === "held-in-one-hand") {
-        isProperlyEquipped = item.isHeld;
-    } else if (item.system.usage.type === "worn") {
-        isProperlyEquipped = item.isWorn;
-    }
-    return isProperlyEquipped && isProperlyInvested;
-}
+// export function checkIfImportantUpdate(item) {
+//     let actions = item.rules.filter((rule) => rule.key === "GrantItem").map(rule => rule.uuid);
+//     if (actions.length === 0) return;
+//     const isProperlyInvested = item.system.traits.value.includes("invested") === !!item.isInvested;
+//     let isProperlyEquipped = false;
+//     if (item.system.usage.value === "held-in-one-hand") {
+//         isProperlyEquipped = item.isHeld;
+//     } else if (item.system.usage.type === "worn") {
+//         isProperlyEquipped = item.isWorn;
+//     }
+//     return isProperlyEquipped && isProperlyInvested;
+// }
 
-export function removeOrAddActions(actor, itemIds, isAdd = true) {
-    const actions = [];
-    for (uuid in itemIds) {
-        let action = await fromUuid(uuid);
-        actions.push(action.toObject());
-    }
-    if (isAdd) {
-        actor.createEmbeddedDocuments("Item", actions);
-    } else {
-        const actionSlugs = actions.map(action => action.slug);
-        actor.deleteEmbeddedDocuments("Item", actor.items.filter(item => actionSlugs.includes(item.slug)).map(item => item.slug));
-    }
-}
+// export function removeOrAddActions(actor, itemIds, isAdd = true) {
+//     const actions = [];
+//     for (uuid in itemIds) {
+//         let action = await fromUuid(uuid);
+//         actions.push(action.toObject());
+//     }
+//     if (isAdd) {
+//         actor.createEmbeddedDocuments("Item", actions);
+//     } else {
+//         const actionSlugs = actions.map(action => action.slug);
+//         actor.deleteEmbeddedDocuments("Item", actor.items.filter(item => actionSlugs.includes(item.slug)).map(item => item.slug));
+//     }
+// }
 
 export function debugLog(data, context = "") {
     if (game.settings.get("pf2e-item-activations", 'debug-mode'))
