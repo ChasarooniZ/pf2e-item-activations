8a9c6e04973a4ab1bfaf1b21a4cb85f4545233a7
cadowtin <chasdowtin@gmail.com>
2024-03-07 14:23:10 -0600

    Fixed import bug

--- scripts/helpers/misc.js
+++ scripts/helpers/misc.js
@@ -1,10 +1,10 @@
-import { ITEM_LIST } from "./item-list";
+import { ITEM_LIST } from "./item-list.js";
 
 export function indexSlugs() {
     const index = game.packs.get("pf2e-item-activations.item-activations").index;
     for (let item in ITEM_LIST) {
         ITEM_LIST[item].slugs = ITEM_LIST[item].actions.map(uuid =>
-            game?.pf2e?.system?.sluggify(index.get(uuid.split('.').slice(-1)[0]).name)
+            game ? .pf2e ? .system ? .sluggify(index.get(uuid.split('.').slice(-1)[0]).name)
         );
     }
 }
